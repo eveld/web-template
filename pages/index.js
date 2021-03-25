ee680ba19f3ec7301f975653db75b2bf1fa943b8
Erik Veld <eveld@hashicorp.com>
2021-03-25 16:06:51 +0100

    remove async

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export async function getStaticProps() {
+export function getStaticProps() {
 	return {
 		props: {
       stage: process.os.STAGE
